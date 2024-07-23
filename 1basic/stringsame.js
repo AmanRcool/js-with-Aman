a0dc223eeb95c9eff5d1978e581196f0fc51aee8
AmanRcool <141349097+AmanRcool@users.noreply.github.com>
2024-07-23 16:08:31 +0000

    A

---
+++ 1basic/stringsame.js
@@ -0,0 +1,14 @@
+const name = "Aman"
+const repoCount =7
+//console.log(name+ repoCount+ " my"); // Aman7 my
+console.log(name.toUpperCase()+ " Hello Your repocount is " +repoCount); //AMAN Hello YOur repocount is 7
+const gamename = new String('AmanRcool')
+console.log(gamename); // [String: 'AmanRcool']
+console.log(gamename.length) //9
+const gn = new String('AmanRcool')
+console.log(gn.charAt(2))  //a 
+console.log(gn.indexOf('A')) //0
+
+
+const st =  gn.substring(0,4)
+console.log(st); //Aman 
