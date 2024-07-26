162e96ea7b2827e03812579c5c85956691ac0e62
AmanRcool <141349097+AmanRcool@users.noreply.github.com>
2024-07-26 22:17:01 +0530

    Add files via upload

---
+++ 1basic/array.js
@@ -0,0 +1,17 @@
+// array
+const array =  [2,3,2,6,8,9]
+console.log(array[5]);// 9
+const  myHero=  ["shaktiman",  "tamrajkilvish"]
+console.log(myHero[1]); // tamrajkilvish
+const num = new Array (1,3,4,5,7)
+console.log(num); // [ 1, 3, 4, 5, 7 ]
+ array.push(12)
+ console.log(array); // [
+    2, 3,  2, 6,
+    8, 9, 12
+//   ]
+array.pop()
+console.log(array); // [ 2, 3, 2, 6, 8, 9 ]
+console.log(array.includes(9)); // true
+console.log(array.indexOf(9)); // 5
+console.log(typeof array.join());// string
